199348659d0b5748a64892013b26ed8b4c093533
dhprlabs <dhprlabs@gmail.com>
2026-04-23 01:44:36 +0530

    render my first triangle

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,13 @@
 #define GLFW_INCLUDE_NONE
-
 #include <GLFW/glfw3.h>
+
 #include "GLEW/GL/glew.h"
 
-// #include "imgui.h"
-// #include "imgui_impl_glfw.h"
-// #include "imgui_impl_opengl3.h"
+/*
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+*/
 
 int main(void)
 {
@@ -31,25 +33,86 @@ int main(void)
         return -1;
 
 
-    // triangle co-ordinates
+    /* triangle co-ordinates */
     float vertices[] = {
         0.5f, -0.5f, 0.0f,
        -0.5f, -0.5f, 0.0f,
         0.0f,  0.5f, 0.0f
     };
 
-    // generating vertex buffer object
+    /* generating vertex buffer object */
     unsigned int VBO;
     glGenBuffers(1, &VBO);
+	// glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	/* simple vertex shader code (GLSL) */
+	const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+	"}\0";
 
-    // Setup Dear ImGui context
+	/* generating vertex shader */
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	/* simple fragment shader code (GLSL) */
+	const char* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\0";
+
+	/* generating fragment shader */
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	
+	/* creating shader program & linking shaders */
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// activating program
+	// glUseProgram(shaderProgram);
+
+	/* delete shader objects */
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// telling opengl how to link vertex data to vertex attributes
+	// glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// glEnableVertexAttribArray(0); 
+	
+	/* generating vertex array object */
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glBindVertexArray(0); 
+    
+	/* Setup Dear ImGui context */
     // IMGUI_CHECKVERSION();
     // ImGui::CreateContext();
     // ImGuiIO& io = ImGui::GetIO();
     // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
-    // Setup Platform/Renderer backends
+    /* Setup Platform/Renderer backends */
     // ImGui_ImplGlfw_InitForOpenGL(window, true); 
     // ImGui_ImplOpenGL3_Init();
 
@@ -57,12 +120,17 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+		glClearColor(0.1f, 0.1f, 0.8f, 1.0f);        
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		/* Simple ImGui Window */
         // ImGui_ImplOpenGL3_NewFrame();
         // ImGui_ImplGlfw_NewFrame();
-        // ImGui::NewFrame();
-        
+        // ImGui::NewFrame();        
 		// ImGui::Begin("Test Window");
 		// ImGui::Button("Click Me :)");
 		// static float rotation = 0.0f;
@@ -71,10 +139,9 @@ int main(void)
 		// ImGui::SliderFloat2("Position", position, -1.0, 1.0);	
 		// static float color[4] = {0.2f, 0.5f, 0.6f, 1.0f};
 		// ImGui::ColorEdit4("Color", color);
-
 		// ImGui::End();
-
-		// ImGui render here
+		
+		/* ImGui render here */
         // ImGui::Render();
         // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         
