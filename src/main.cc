fd452fc362626608df4d53d1930ce7fb33f7dac6
dhprlabs <dhprlabs@gmail.com>
2026-04-28 01:53:25 +0530

    add element buffer object code & rendered a rectangle

--- src/main.cc
+++ src/main.cc
@@ -34,13 +34,32 @@ int main(void)
 
 
     /* triangle co-ordinates */
-    float vertices[] = {
-        0.5f, -0.5f, 0.0f,
-       -0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };
+    // float vertices[] = {
+    //     0.5f, -0.5f, 0.0f,
+    //    -0.5f, -0.5f, 0.0f,
+    //     0.0f,  0.5f, 0.0f
+    // };
 
-    /* generating vertex buffer object */
+	/* rectangle vertices */
+	float vertices[] = {
+		 0.5f,  0.5f, 0.0f,  // top right
+     	 0.5f, -0.5f, 0.0f,  // bottom right
+    	-0.5f, -0.5f, 0.0f,  // bottom left
+    	-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	unsigned int indices[] = { 
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	/* generating element buffer object */
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+    
+	/* generating vertex buffer object */
     unsigned int VBO;
     glGenBuffers(1, &VBO);
 	// glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -97,13 +116,15 @@ int main(void)
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
-    glBindVertexArray(0); 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0); 
     
 	/* Setup Dear ImGui context */
     // IMGUI_CHECKVERSION();
@@ -125,8 +146,11 @@ int main(void)
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);	
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
+		// glBindVertexArray(0);
+	
 		/* Simple ImGui Window */
         // ImGui_ImplOpenGL3_NewFrame();
         // ImGui_ImplGlfw_NewFrame();
